9a241f98fdbafef447a6f0477ffb17dcf67597b7
DirectorLord <thelord161203@gmail.com>
2025-08-06 10:22:33 +0300

    Print extension method was made

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -1,11 +1,17 @@
-﻿namespace LINQ01;
+﻿using System.Runtime.CompilerServices;
+
+namespace LINQ01;
+
 internal class Program
-{
-    public static void Main(String[] args)
+{   
+    public static void Main()
+    //made the printing method in the Methods file so i can print it and to make the data to be shown
+    //this is the result of the data without the print
+    //System.Linq.Enumerable+ListWhereIterator`1[LINQ01.Product]
+    //what is this??????
     {
-        int x = 0;
-        int y = 0;
-        Console.WriteLine(x);
-        Console.WriteLine(y);
+        var result = ListGenerators.ProductList.Where(p => p.UnitsInStock == 0);
+
+        Console.WriteLine(result);
     }
 }
