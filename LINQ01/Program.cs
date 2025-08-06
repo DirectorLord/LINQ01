c24a8f001a93c865102e16213f0ee8b0dbc1ad29
DirectorLord <thelord161203@gmail.com>
2025-08-06 10:53:23 +0300

    forgot to write down the question

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -11,18 +11,21 @@ internal class Program
     //what is this??????
     {
         #region Question1
+        //Find all products that are out of stock.
         var outOfStock = ListGenerators.ProductList.Where(p => p.UnitsInStock == 0);
 
         outOfStock.Print();
         #endregion
 
         #region Question2
+        //Find all products that are in stock and cost more than 3.00 per unit.
         var priceProducts = ListGenerators.ProductList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3.00M);
 
         priceProducts.Print();
         #endregion
 
         #region Question3
+        //Returns digits whose name is shorter than their value.
         var digits = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
         var result = digits.Where( d => d.Length < Array.IndexOf(digits, d) );
@@ -30,14 +33,17 @@ internal class Program
         #endregion
 
         #region Question4
+        //Get first Product out of Stock 
         var first = ListGenerators.ProductList.FirstOrDefault(p => p.UnitsInStock == 0); first.Print();
         #endregion
 
         #region Question5
+        //Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
         var firstExp = ListGenerators.ProductList.FirstOrDefault(p => p.UnitPrice == 1000); firstExp.Print();
         #endregion
 
         #region Question6
+        // Retrieve the second number greater than 5 
         var skip = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
         var resSkip = skip.Where(n => n>5).FirstOrDefault();
