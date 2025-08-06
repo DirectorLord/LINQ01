47c4fb7150e9989e5554bab3ccf0b5637b6ca543
DirectorLord <thelord161203@gmail.com>
2025-08-06 09:45:49 +0300

    test

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LINQ01;
+internal class Program
+{
+    public static void Main(String[] args)
+    {
+        int x = 0;
+        int y = 0;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+    }
+}
